8e0d0e0a6e96b52dcab4de8d422fa87e944a16c4
henrytorres14 <104407050+henrytorres14@users.noreply.github.com>
2023-04-16 21:28:44 -0500

    Add files via upload

---
+++ Controllers/PersonaController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SebastianRuiz_HenryTorres_EntregaAula2.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SebastianRuiz_HenryTorres_EntregaAula2.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Persona()
+        {
+            string nombre, apellidos, eps, regimen, afiliacion, historiaclinica, enfermedadrelevante, cedula, semanascotizadas, costotratamiento, cantidadenfermedades, edad;
+            nombre = Request.Form["nombre"].ToString();
+            apellidos = Request.Form["apellidos"].ToString();
+            cedula = Request.Form["cedula"].ToString();
+            eps = Request.Form["eps"].ToString();
+            regimen = Request.Form["regimen"].ToString();
+            afiliacion = Request.Form["afiliacion"].ToString();
+            historiaclinica = Request.Form["historiaclinica"].ToString();
+            enfermedadrelevante = Request.Form["enfermedadrelevante"].ToString();
+            semanascotizadas = Request.Form["semanascotizadas"].ToString();
+            costotratamiento = Request.Form["costotratamiento"].ToString();
+            cantidadenfermedades = Request.Form["cantidadenfermedades"].ToString();
+            edad = Request.Form["edad"].ToString();
+
+            Persona persona = new Persona(nombre, apellidos, cedula, eps, regimen, afiliacion, historiaclinica, enfermedadrelevante, semanascotizadas, costotratamiento, cantidadenfermedades, edad);
+            return View(persona);
+        }
+    }
+}
---
+++ Models/Persona.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace SebastianRuiz_HenryTorres_EntregaAula2.Models
+{
+	public class Persona
+	{
+		private string nombre;
+		private string apellidos;
+		private string cedula;
+		private string eps;
+		private string regimen;
+		private string afiliacion;
+		private string historiaclinica;
+		private string enfermedadrelevante;
+		private string semanascotizadas;
+		private string costotratamiento;
+		private string cantidadenfermedades;
+        public string edad;
+
+#pragma warning disable CS8618 // Un campo que no acepta valores NULL debe contener un valor distinto de NULL al salir del constructor. Considere la posibilidad de declararlo como que admite un valor NULL.
+        public Persona(string nombre, string apellidos, string cedula, string eps, string regimen, string afiliacion, string historiaclinica, string enfermedadrelevante, string semanascotizadas, string costotratamiento, string cantidadenfermedades, string edad)
+#pragma warning restore CS8618 // Un campo que no acepta valores NULL debe contener un valor distinto de NULL al salir del constructor. Considere la posibilidad de declararlo como que admite un valor NULL.
+        {
+            this.Nombre = nombre;
+            this.Apellidos = apellidos;
+            this.Cedula = cedula;
+            this.Eps = eps;
+            this.Regimen = regimen;
+            this.Afiliacion = afiliacion;
+            this.Historiaclinica = historiaclinica;
+            this.Enfermedadrelevante = enfermedadrelevante;
+            this.Semanascotizadas = semanascotizadas;
+            this.Costotratamiento = costotratamiento;
+            this.Cantidadenfermedades = cantidadenfermedades;
+            this.Edad = edad;
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellidos { get => apellidos; set => apellidos = value; }
+        public string Cedula { get => cedula; set => cedula = value; }
+        public string Eps { get => eps; set => eps = value; }
+        public string Regimen { get => regimen; set => regimen = value; }
+        public string Afiliacion { get => afiliacion; set => afiliacion = value; }
+        public string Historiaclinica { get => historiaclinica; set => historiaclinica = value; }
+        public string Enfermedadrelevante { get => enfermedadrelevante; set => enfermedadrelevante = value; }
+        public string Semanascotizadas { get => semanascotizadas; set => semanascotizadas = value; }
+        public string Costotratamiento { get => costotratamiento; set => costotratamiento = value; }
+        public string Cantidadenfermedades { get => cantidadenfermedades; set => cantidadenfermedades = value; }
+        public string Edad { get => edad; set => edad = value; }
+    }
+}
+
